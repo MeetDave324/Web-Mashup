fd14069fcbe35510d2ecf8d3ad499a9b739146ee
MeetDave324 <47854189+MeetDave324@users.noreply.github.com>
2021-07-06 12:05:38 +0530

    Update IBM WATSON.py

--- IBM WATSON.py
+++ IBM WATSON.py
@@ -8,12 +8,12 @@ import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import requests
-authenticator = IAMAuthenticator('EtYKFV6o4oYqhJ8RU_gMwBzw_a0Ax3k7iR0KAng5T4HQ')
+authenticator = IAMAuthenticator('Your Key')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
-language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9b5e188b-8981-4c9b-bb97-62d8a5d596ea')
+language_translator.set_service_url('Your Key')
 
 BASE_LANGUAGE = 'en'
 print('Enter the Word')
